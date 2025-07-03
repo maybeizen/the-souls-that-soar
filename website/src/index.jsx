972f3dcbe57afe16863a6a02c256621837ba5cff
maybeizen <briarchamberlin1@gmail.com>
2025-07-03 01:32:18 -0400

    fixes

--- website/src/index.jsx
+++ website/src/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import "./assets/css/global.css";
+import "/fa/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
